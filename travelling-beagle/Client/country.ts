88cc085a6823826c3c5af98118f7be47094d7537
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-11-05 19:57:51 -0500

    Click on image to make it bigger

---
+++ travelling-beagle/Client/country.ts
@@ -0,0 +1,14 @@
+﻿const countryImages = document.getElementsByClassName('country-image-src');
+const overlay = <HTMLDivElement>document.getElementById("overlay");
+const overlayImage = <HTMLImageElement>document.getElementById("overlayImage");
+
+for (let i = 0; i < countryImages.length; i++) {
+    const img = <HTMLImageElement>countryImages.item(i);
+    img.onclick = () => {
+        overlayImage.src = img.src;
+        overlay.style.display = 'block';
+        overlayImage.style.left = `${window.innerWidth / 2 - img.width}px`;
+    };
+}
+
+overlay.onclick = () => overlay.style.display = 'none';
